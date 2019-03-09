0c5f991f2a239ac9ba512e38b308795f843179a8
TRUZijunChen <benjamin9527@icloud.com>
2019-03-09 11:06:08 -0800

    capture edge done

--- app/src/main/java/com/example/benjamin/numberrecognition/MainActivity.java
+++ app/src/main/java/com/example/benjamin/numberrecognition/MainActivity.java
@@ -17,9 +17,13 @@ import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
@@ -89,30 +93,33 @@ public class MainActivity extends AppCompatActivity {
             Mat grayMat = new Mat();
             Mat binMat = new Mat();
             Mat image_dil = new Mat();
+            Mat middle = new Mat();
             BitmapFactory.Options o = new BitmapFactory.Options();
             o.inSampleSize = 4;
 
             int width = imageBitmap.getWidth();
             int heigh = imageBitmap.getHeight();
-            Bitmap grayBitmap = Bitmap.createBitmap(width, heigh, Bitmap.Config.RGB_565);
+            Bitmap result = Bitmap.createBitmap(width, heigh, Bitmap.Config.RGB_565);
 
             //bitmap to mat
             Utils.bitmapToMat(imageBitmap, Rgba);
             Imgproc.cvtColor(Rgba, grayMat, Imgproc.COLOR_BGR2GRAY);
-            Utils.matToBitmap(grayMat, grayBitmap);
-
-
-
+            Utils.matToBitmap(grayMat, result);
             Imgproc.GaussianBlur(grayMat, grayMat, new Size(5, 5),0.0);
-            Imgproc.threshold(grayMat, binMat, 80, 255, THRESH_BINARY_INV);
-//            Utils.matToBitmap(binMat, grayBitmap);
-//            picture.setImageBitmap(grayBitmap);
+            Imgproc.Canny(grayMat, grayMat, 80, 200);
+            Utils.matToBitmap(grayMat, result);
+            picture.setImageBitmap(result);
 
-            final Size kernelSize = new Size(9, 9);
+
+            Imgproc.threshold(grayMat, binMat, 80, 255, THRESH_BINARY_INV);
+//            Utils.matToBitmap(binMat, result);
+//            picture.setImageBitmap(result);
+
+            final Size kernelSize = new Size(3, 3);
             Mat element = Imgproc.getStructuringElement(MORPH_RECT, kernelSize);
             Imgproc.dilate(binMat, image_dil, element);
-            Utils.matToBitmap(image_dil, grayBitmap);
-            picture.setImageBitmap(grayBitmap);
+//            Utils.matToBitmap(image_dil, grayBitmap);
+//            picture.setImageBitmap(grayBitmap);
 
 
             List<MatOfPoint> contours_out = new ArrayList<>();
@@ -120,10 +127,36 @@ public class MainActivity extends AppCompatActivity {
             Imgproc.findContours(image_dil, contours_out, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
             int size = contours_out.size();
             ArrayList<Rect> num_location = new ArrayList<>();
+
+
             for(int i = 0; i < size; i++){
-                num_location.add(boundingRect(contours_out.get(i)));
+//                num_location.add(boundingRect(contours_out.get(i)));
+
+
+
+                // Minimum size allowed for consideration
+                MatOfPoint2f approxCurve = new MatOfPoint2f();
+                MatOfPoint2f contour2f = new MatOfPoint2f( contours_out.get(i).toArray() );
+                //Processing on mMOP2f1 which is in type MatOfPoint2f
+                double approxDistance = Imgproc.arcLength(contour2f, true)*0.02;
+                Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
+
+                //Convert back to MatOfPoint
+                MatOfPoint points = new MatOfPoint( approxCurve.toArray() );
+
+                // Get bounding rect of contour
+                Rect rect = Imgproc.boundingRect(points);
+
+                Imgproc.rectangle(image_dil, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(255, 0, 0, 255), 3);
+
+
+
             }
 
+//            Utils.matToBitmap(image_dil, grayBitmap);
+//            picture.setImageBitmap(grayBitmap);
         }
+
+
     }
 }
