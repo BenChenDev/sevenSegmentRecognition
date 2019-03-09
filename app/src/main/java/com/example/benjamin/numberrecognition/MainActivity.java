6e795362fdc346bf3d9e6a66eabd0c7173f04276
TRUZijunChen <benjamin9527@icloud.com>
2019-03-08 23:56:43 -0800

    findcontour half way done.

--- app/src/main/java/com/example/benjamin/numberrecognition/MainActivity.java
+++ app/src/main/java/com/example/benjamin/numberrecognition/MainActivity.java
@@ -18,8 +18,22 @@ import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.opencv.imgproc.Imgproc.CHAIN_APPROX_NONE;
+import static org.opencv.imgproc.Imgproc.CHAIN_APPROX_SIMPLE;
+import static org.opencv.imgproc.Imgproc.MORPH_RECT;
+import static org.opencv.imgproc.Imgproc.RETR_EXTERNAL;
+import static org.opencv.imgproc.Imgproc.THRESH_BINARY;
+import static org.opencv.imgproc.Imgproc.THRESH_BINARY_INV;
+import static org.opencv.imgproc.Imgproc.boundingRect;
+import static org.opencv.imgproc.Imgproc.findContours;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -73,6 +87,8 @@ public class MainActivity extends AppCompatActivity {
 
             Mat Rgba = new Mat();
             Mat grayMat = new Mat();
+            Mat binMat = new Mat();
+            Mat image_dil = new Mat();
             BitmapFactory.Options o = new BitmapFactory.Options();
             o.inSampleSize = 4;
 
@@ -88,7 +104,26 @@ public class MainActivity extends AppCompatActivity {
 
 
             Imgproc.GaussianBlur(grayMat, grayMat, new Size(5, 5),0.0);
+            Imgproc.threshold(grayMat, binMat, 80, 255, THRESH_BINARY_INV);
+//            Utils.matToBitmap(binMat, grayBitmap);
+//            picture.setImageBitmap(grayBitmap);
+
+            final Size kernelSize = new Size(9, 9);
+            Mat element = Imgproc.getStructuringElement(MORPH_RECT, kernelSize);
+            Imgproc.dilate(binMat, image_dil, element);
+            Utils.matToBitmap(image_dil, grayBitmap);
             picture.setImageBitmap(grayBitmap);
+
+
+            List<MatOfPoint> contours_out = new ArrayList<>();
+            Mat hierarchy = new Mat();
+            Imgproc.findContours(image_dil, contours_out, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+            int size = contours_out.size();
+            ArrayList<Rect> num_location = new ArrayList<>();
+            for(int i = 0; i < size; i++){
+                num_location.add(boundingRect(contours_out.get(i)));
+            }
+
         }
     }
 }
